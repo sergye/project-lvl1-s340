3b61e482a16f31dcc8e05044229aa0297c78eb62
sergye <sergye@gmail.com>
2018-09-10 11:44:00 +0300

    remove unnecesary console.log

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const greetUser = () => {
-  console.log('Welcome to the Brain Games!');
-  console.log();
+  console.log('Welcome to the Brain Games!\n');
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello, ${userName}!`);
 };
