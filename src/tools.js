972d908b7d48ad608fdd886912ab8afc1fabb10c
sergye <sergye@gmail.com>
2018-09-11 11:20:11 +0300

    create common tools file

---
+++ src/tools.js
@@ -0,0 +1,19 @@
+export const getRandom = (min = 0, max = 99) => (Math.floor(Math.random() * (max - min))) + min;
+
+export const getOperator = (num) => {
+  switch (num) {
+    case 0: return '+';
+    case 1: return '-';
+    case 2: return '*';
+    default: return 'null';
+  }
+};
+
+export const calculate = (oper, num1, num2) => {
+  switch (oper) {
+    case '+': return (num1 + num2);
+    case '-': return (num1 - num2);
+    case '*': return (num1 * num2);
+    default: return 'null';
+  }
+};
