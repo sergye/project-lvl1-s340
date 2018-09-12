7b23d327eb95c519763a4e9b66ee5c2ee212082d
sergye <sergye@gmail.com>
2018-09-12 08:08:40 +0300

    add brain-balance functions

--- src/tools.js
+++ src/tools.js
@@ -1,4 +1,4 @@
-export const getRandom = (min = 0, max = 99) => (Math.floor(Math.random() * (max - min))) + min;
+export const getRandom = (min = 0, max = 999) => (Math.floor(Math.random() * (max - min))) + min;
 
 export const getOperator = (num) => {
   switch (num) {
@@ -21,4 +21,31 @@ export const calculate = (oper, num1, num2) => {
 export const getGCD = (n, m) => {
   if (n === m) return n;
   return (n < m) ? getGCD(n, m - n) : getGCD(m, n - m);
+};
+
+const getSumOfDigits = (number) => {
+  const numberStr = String(number);
+  const strLength = numberStr.length;
+  const iterSum = (str, acc, count) => {
+    if (count < 0) {
+      return acc;
+    }
+    const sum = acc + Number(str[count]);
+    return iterSum(str, sum, count - 1);
+  };
+  return iterSum(numberStr, 0, strLength - 1);
+};
+
+export const balanceNumber = (number) => {
+  const sumOfDigits = getSumOfDigits(number);
+  const digitsNumber = String(number).length;
+  const iterBalance = (sum, count, result) => {
+    if (count === 0) {
+      return Number(result);
+    }
+    const newDigit = Math.floor(sum / count);
+    const newResult = result + String(newDigit);
+    return iterBalance(sum - newDigit, count - 1, newResult);
+  };
+  return iterBalance(sumOfDigits, digitsNumber, '');
 };
